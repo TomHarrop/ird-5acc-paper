3500a75237b39b3ffdf754c345be3496a21b423f
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-05-03 18:36:37 +0200

    Refine ap2 selection for fluidigm

--- src/fig-select-new-fluidigm.R
+++ src/fig-select-new-fluidigm.R
@@ -4,7 +4,12 @@ library(DESeq2)
 source("helper-functions.R")
 
 
-selected <- read_excel("../data-raw/candidate_new_fluidigm.xlsx")
+# selected <- read_excel("../data-raw/candidate_new_fluidigm.xlsx") %>%
+#   arrange(locus_id)
+selected <- read_csv("../selected_genes/candidate_new_fluidigm.csv") %>%
+  arrange(locus_id) %>%
+  select(-X1)
+
 dds <- readRDS("../data-raw/dds.Rds")
 
 selected_expr <- get_expression(locus_ids = selected$locus_id,
