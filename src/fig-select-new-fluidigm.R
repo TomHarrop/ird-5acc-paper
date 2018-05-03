bc1dadd6d6dcab126586043208bf192fe4c79623
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-05-03 12:24:01 +0200

    Draft gene selection for new fluidigm round
    We need it to test hypothesis of delayed
    differentiation

---
+++ src/fig-select-new-fluidigm.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(readxl)
+library(DESeq2)
+source("helper-functions.R")
+
+
+selected <- read_excel("../data-raw/candidate_new_fluidigm.xlsx")
+dds <- readRDS("../data-raw/dds.Rds")
+
+selected_expr <- get_expression(locus_ids = selected$locus_id,
+                           dds = dds) %>%
+  left_join(selected)
+
+
+
+pdf(file = "../fig/fig-select-new-fluidigm.pdf",
+    width = 16, height = 14)
+plot_norm_expr(selected_expr) +
+  facet_wrap(facets = c("locus_id",
+                        "gene_name",
+                        "why_interesting"),
+             scales = "free_y",
+             ncol = 5,
+             labeller = label_wrap_gen(width = 42))
+dev.off()
+
