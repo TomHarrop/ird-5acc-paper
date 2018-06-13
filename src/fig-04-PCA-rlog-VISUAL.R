3fca0e8e74cc9c12821b7206c7a53bb2322b4796
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-06-13 15:07:07 +0200

    Visualize PC-rlog - FIG-04
    One dimensional representation
    as suggested by Tom?

--- src/fig-04-PCA-rlog-VISUAL.R
+++ src/fig-04-PCA-rlog-VISUAL.R
@@ -1,2 +1,22 @@
 library(tidyverse)
 load("../data/rlog-pca.Rdata")
+color_palette <- c("blue", "goldenrod")
+
+pcx <- pcx %>%
+  select_at(vars(accession:PC5)) %>%
+  gather(PC1:PC5, key = "PC_id", value = "pc_x")
+
+pdf("../fig/fig-04-PCA-rlog-VISUAL.pdf")
+ggplot(pcx,
+       aes(x = ID,
+           fill = stage,
+           # colour = stage,
+           y = pc_x)) +
+  geom_col() +
+  # geom_point(size = 4) +
+  geom_hline(yintercept = 0) +
+  facet_grid(PC_id ~ accession, scales = "free") +
+  theme_bw() +
+  # scale_color_manual(values = color_palette) 
+  scale_fill_manual(values = color_palette)
+dev.off()
