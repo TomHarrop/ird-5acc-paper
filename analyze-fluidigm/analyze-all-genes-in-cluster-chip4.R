7992316327564f70e9f4454b1e071d92c19c3137
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-08-02 21:16:49 +0200

    Notes on ap2s

---
+++ analyze-fluidigm/analyze-all-genes-in-cluster-chip4.R
@@ -0,0 +1,108 @@
+library(tidyverse)
+library(readxl)
+library(pheatmap)
+library(viridis)
+
+source("helper_functions_fluidigm.R")
+
+# load fluidigms -------------------------------------------------------
+
+fluidigms <- c(hb = "../data-raw/fluidigm_ok/HBok_CHIP4.xlsx",
+               horm_exp = "../data-raw/fluidigm_ok/HormoneOK_CHIP4.xlsx",
+               mads = "../data-raw/fluidigm_ok/MADSok_CHIP4.xlsx",
+               spl = "../data-raw/fluidigm_ok/SPok_CHIP4.xlsx",
+               ref_exp = "../data-raw/fluidigm/HK_CHIP4.xlsx") %>%
+  map(., read_fluidigm)
+
+# Analyze normalizers -----------------------------------------------------
+
+norms <- fluidigms$ref_exp %>%
+  group_by(sample_name) %>%
+  summarize(norm_geom_mean = gm_mean(ct_value))
+
+# Merge results -----------------------------------------------------------
+
+chip4_exp <- fluidigms; chip4_exp$ref_exp <- NULL
+chip4_exp <- chip4_exp %>% purrr::reduce(bind_rows)
+
+# merge normalizers and estimate expression -------------------------------
+
+chip4_exp <- chip4_exp %>%
+  left_join(norms)  %>%
+  # subtract normalizer and take exponential to estimate expression
+  # note!!! Skip calibration, is it legit????
+  # So the formula is 2^-(Ct_gene - Ct_norm)
+  mutate(expression = 2^(-(ct_value - norm_geom_mean))) %>%
+  # Low expressed genes (Ct 999) to 0
+  mutate(expression = round(expression, digits = 5))
+
+# Plot both RNAseq and fluidigm -------------------------------------------
+
+library(DESeq2)
+library(gridExtra)
+library(grid)
+
+source("../src/helper-functions.R")
+
+dds <- readRDS("../data-raw/dds.Rds")
+
+chip4_rnaseq <- unique(chip4_exp$locus_id) %>%
+  get_expression(dds = dds) %>%
+  mutate(species = factor(species,
+                          levels = c("japonica", 
+                                     "barthii",
+                                     "glaberrima", 
+                                     "rufipogon", 
+                                     "indica"))) 
+
+chip4_exp <- chip4_exp %>% 
+  scale_tidy_fluidigm() %>%
+  mutate(species = factor(species,
+                          levels = c("Osj", 
+                                     "Ob",
+                                     "Og", 
+                                     "Or", 
+                                     "Osi"))) 
+# missing locus ids
+
+# LOC_Os10g41231, LOC_Os10g41232, LOC_Os10g41233, LOC_Os10g41234,
+# LOC_Os10g41235, LOC_Os10g41236, LOC_Os10g41237, LOC_Os10g41238,
+# LOC_Os10g41239, LOC_Os10g41241, LOC_Os10g41242, LOC_Os10g41243,
+# LOC_Os10g41244, LOC_Os10g41245, LOC_Os10g41246, LOC_Os10g41247,
+# LOC_Os10g41248, LOC_Os10g41249, LOC_Os10g41251, LOC_Os10g41252,
+# LOC_Os10g41253, LOC_Os10g41254, LOC_Os10g41255, LOC_Os10g41256,
+# LOC_Os10g41257, LOC_Os10g41258, LOC_Os10g41259, LOC_Os10g41261,
+# LOC_Os10g41262, LOC_Os10g41263, LOC_Os10g41264, LOC_Os10g41265,
+# LOC_Os10g41266, LOC_Os10g41267, LOC_Os10g41268, LOC_Os10g41269,
+# LOC_Os10g41270, LOC_Os10g41271, LOC_Os10g41272, LOC_Os10g41273,
+# LOC_Os10g41274, LOC_Os10g41275, LOC_Os10g41276, LOC_Os10g41277,
+# LOC_Os10g41278, LOC_Os10g41279, LOC_Os10g41281, LOC_Os10g41282,
+# LOC_Os10g41283, LOC_Os10g41284, LOC_Os10g41285, LOC_Os10g41286, 
+# LOC_Os10g41287, LOC_Os10g41288, LOC_Os10g41289, LOC_Os12g01120, 
+# LOC_OS02g52340
+
+
+# Check distribution ------------------------------------------------------
+
+ggplot(chip4_exp, aes(x = sample_name,
+                      y = expression,
+                      fill = species)) +
+  geom_boxplot() +
+  ggplot2::scale_y_log10()
+
+# id_fluidigm <- "LOC_Os06g47150"
+# rnaseq_dat <- clust_rnaseq
+
+
+# plot all ----------------------------------------------------------------
+
+
+pdf(file = "../fig/chip4-fluidigm-rnaseq.pdf",
+    height = 7)
+unique(chip4_rnaseq$locus_id) %>% map(~plot_both(.,
+                                                 fluidigm_dat = chip4_exp,
+                                                 rnaseq_dat = chip4_rnaseq))
+dev.off()
+
+
+# Interesting ones --------------------------------------------------------
---
+++ notes/ap2-story.md
@@ -0,0 +1,78 @@
+If we draft the paper on AP2s-ERF
+
+# AP2-ERF - what is known?
+
+stress, but stress is connected to flowering through drought-escape. Are they mis-annotated?
+
+
+### initiation of crown root - PLETORA2- CRL5 - LOC_Os07g03250
+
+crown rootless5 (crl5) produced fewer crown roots impaired initiation of crown root primordia (YUKA KITOMY 2011)
+
+expr higher in indica Branch Meristem
+
+Also **CRl1 LOC_Os03g05510 ARL1** higher in indica
+
+Not in clusters
+
+### Rice Starch regulation RSR1 LOC_Os05g03040
+
+Seed development - starch defect in mutants. (Fu 2010)
+
+Expressed in SM
+
+### OsEATB LOC_Os09g28440
+
+https://funricegenes.github.io/OsEATB/
+
+tillering and panicle branching in Indica through gibberellin (Qi et al., 2011)
+
+**Not detected? We need to look at this more in detail**
+
+Connected with NAC2, which is expressed in our dataset, but not differntially epxressed
+
+### SNB LOC_Os07g13170
+
+https://funricegenes.github.io/SNB/
+
+Transition spikelet to floral meristem (Lee et al., 2007)
+
+expressed but maybe not DE
+
+not in clusters
+
+### IDS1  LOC_Os03g60430
+
+https://funricegenes.github.io/OsIDS1/
+
+Transition spikelet to floral meristem and also branching with SNB (same paper)
+
+expressed as IDS1 but DE between stages more marked
+
+In Cluster 1
+
+### DREB4 LOC_Os04g46400
+
+https://funricegenes.github.io/OsDREB4-2~CR250/
+
+expression of OsDREB4-1 was induced by dehydration and high salt, whereas OsDREBl-l and OsDREB4-2 were expressed constitutively (Tian et al,2005)
+
+**could this be a case of drought excape, and this just a developmental regulator?**
+
+higher in BM
+
+### OsEREBP1 LOC_Os02g54160
+
+https://funricegenes.github.io/OsEREBP1/
+
+Something stress regulated
+
+**same as above?**
+
+Defenitely expressed, higher in spikelets
+
+### MULTI-FLORET SPIKELET1 LOC_Os05g041760
+
+Spikelet to flower meristem? (Ren et al., 2013)
+
+**Not detected? Check better**
