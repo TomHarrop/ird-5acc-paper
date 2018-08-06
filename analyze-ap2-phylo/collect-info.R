f4999a9bed33d7b63d876adc5fb73723af3209e0
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-08-06 18:35:39 +0200

    Plot ap2s with additional genes from Nakano 2006

--- analyze-ap2-phylo/collect-info.R
+++ analyze-ap2-phylo/collect-info.R
@@ -1,12 +1,13 @@
 library(tidyverse)
-
+library(magrittr)
 
 # Get families ------------------------------------------------------------
 
 fams <- "https://funricegenes.github.io/famInfo.table.txt"
 
 fams <- fams %>%
-  read_delim(delim = "\t")
+  read_delim(delim = "\t") %>%
+  mutate(locus_id = MSU)
 
 
 
@@ -18,7 +19,8 @@ dest <- "../data-raw/ap2s-nakano.xls"
 
 if(!file.exists(dest)) ap2s %>% download.file(destfile = dest)
 
-ap2s <- readxl::read_excel(path = dest, range = "A2:G141")
+ap2s <- readxl::read_excel(path = dest, range = "A2:G141") %>%
+  mutate(locus_id = paste0("LOC_", `TIGR Locus identifier`))
 
 # Get ap2 subfams ---------------------------------------------------------
 
@@ -27,5 +29,31 @@ ap2_subfam <- c("PLT",
                 "DREB",
                 "AP2")
 
-fams %>%
+fams_erf <- fams %>%
   filter(Name %in% ap2_subfam)
+
+all_ap2s <- fams_erf %>% 
+  full_join(ap2s) %>%
+  write_csv("../data/ap2_full.csv")
+
+
+# Plot all ap2s -----------------------------------------------------------
+
+all_ap2s <- all_ap2s %>% 
+  select(locus_id, Name, `Generic Name`) %>%
+  dplyr::rename(generic_name = `Generic Name`)
+
+library(DESeq2)
+
+source("../src/helper-functions.R")
+dds <- readRDS(file = "../data-raw/dds.Rds")
+
+pdf("../fig/more_ap2s.pdf", height = 50, width = 20)
+all_ap2s %$%
+  get_expression(locus_id, dds = dds) %>%
+  left_join(all_ap2s) %>%
+  plot_norm_expr() +
+  facet_wrap(facets = c("locus_id", "Name", "generic_name"),
+             scales = "free_y",
+             ncol = 5)
+dev.off()  
