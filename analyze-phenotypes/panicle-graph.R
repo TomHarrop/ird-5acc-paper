a3e161e139bd0935c679beaa22c652129d6386b4
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-09-17 18:38:29 +0200

    test tidygraph for phenotype viz

--- analyze-phenotypes/panicle-graph.R
+++ analyze-phenotypes/panicle-graph.R
@@ -2,15 +2,31 @@
 library(tidyverse)
 library(magrittr)
 library(xml2)
+# library(tidygraph)
+library(ggraph)
 
 vertices <- read_xml("../data-raw/pheno-xml/Nip_1_1_6307.ricepr") %>%
   # xml_child("graph")  %>%
   # xml_child("vertices") %>%
   xml_find_all(".//vertex")
   
-dat <- tibble(x = as.numeric(xml_attr(vertices, "x")),
+nodes <- tibble(x = as.numeric(xml_attr(vertices, "x")),
        y = as.numeric(xml_attr(vertices, "y")),
-       type = xml_attr(vertices, "type"))
+       type = xml_attr(vertices, "type"),
+       id = xml_attr(vertices, "id"))
+
+edges <- read_xml("../data-raw/pheno-xml/Nip_1_1_6307.ricepr") %>%
+  xml_find_all(".//edge")
+
+edges <- tibble(from = xml_attr(edges, "vertex1"),
+                to = xml_attr(edges, "vertex2"))
+
+tst <- tidygraph::tbl_graph(nodes = nodes,
+                            edges = edges, )
+  
+ggraph(tst)+ 
+  geom_edge_link() + 
+  geom_node_point(size = 8, colour = 'steelblue') 
 
 ggplot(dat,
        aes(x = x,
