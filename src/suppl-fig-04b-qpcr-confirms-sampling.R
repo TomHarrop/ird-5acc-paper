e63a69f38cbe192ad57f9208f893038f72e54ea4
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-10-16 16:24:34 +0200

    impute 0 in supplementary qpcr plot

--- src/suppl-fig-04b-qpcr-confirms-sampling.R
+++ src/suppl-fig-04b-qpcr-confirms-sampling.R
@@ -2,10 +2,15 @@ library(tidyverse)
 library(viridis)
 library(pheatmap)
 library(grid)
-library(gridExtra)
+library(gridExtra) 
+library(magrittr)
 
 
 source("../analyze-fluidigm/helper_functions_fluidigm.R")  
+
+
+# Load file ---------------------------------------------------------------
+
 
 
 load(file = "../data/fluidigm-confim-sampling.Rdata")
@@ -17,39 +22,66 @@ qpcr_exp <- bind_rows(chip3_exp, chip4_exp) %>%
                                      "Ob", "Og",
                                      "Or", "Osi")))
 
-lineplot_fluidigm(nm = "Sampling Check",
-                  dat = qpcr_exp,
-                  expr_val = expression) +
-  scale_y_log10() +
-  facet_wrap(facets = c('target_name', "species"),
-             scales = "free_y")
 
-pdf("../fig/suppl-fig-04b-qpcr-confirms-sampling.pdf",
-    height = 6, width = 6)
-lineplot_fluidigm(nm = "Sampling Check",
-                  dat = qpcr_exp) +
+# Impute 0/ND -------------------------------------------------------------
+
+# What is a the minimal expression besides 0?
+# btw 0 must be intended as Non Detected.
+
+# set it to one order of magnitude smaller than
+# the minimum that was analyzed for a given amplicon
+
+impute_zero <- function(expr) {
+  min_expr <- 
+    expr[expr > 0] %>%
+    min()
+  return(min_expr/10)
+} 
+
+
+qpcr_imputed <- 
+  qpcr_exp %>% 
+  group_by(target_name) %>%
+  mutate(expression = case_when(expression == 0 ~ impute_zero(expression),
+                                TRUE ~ expression)) 
+  
+
+# Plot --------------------------------------------------------------------
+
+p <- 
+  qpcr_imputed %>%
+  {lineplot_fluidigm(nm = "Sampling Check", dat = .)} +
   scale_y_log10() +
-  labs(title = "Marker genes behave as expected [axis y log10]")
+  labs(title = "Marker genes behave as expected [semi-log plot]")
+
+pdf("../fig/suppl-fig-qpcr-confirms-sampling.pdf",
+    height = 6, width = 6,
+    paper = "a4")
+p %>% print()
 dev.off()
 
-pdf("../fig/suppl-fig-04b-qpcr-confirms-sampling-free-y.pdf",
-    height = 9,
-    width = 9)
-lineplot_fluidigm(nm = "Sampling Check",
-                  dat = qpcr_exp,
-                  expr_val = expression) +
-  scale_y_log10() +
-  facet_wrap(facets = c('target_name', "species"),
-             scales = "free_y") +
-  labs(title = "Marker genes behave as expected [axis y log10]")
-dev.off()
 
-pdf("../fig/suppl-fig-04b-qpcr-confirms-sampling-scaled.pdf",
-    height = 6,
-    width = 6)
-lineplot_fluidigm(nm = "Sampling Check",
-                  dat = qpcr_exp,
-                  expr_val = expr_scale_gene_spec_0_10) +
-  scale_y_log10() +
-  labs(title = "Marker genes behave as expected [axis y log10]")
-dev.off()
+# OLD ---------------------------------------------------------------------
+
+ 
+# pdf("../fig/suppl-fig-04b-qpcr-confirms-sampling-free-y.pdf",
+#     height = 9,
+#     width = 9)
+# lineplot_fluidigm(nm = "Sampling Check",
+#                   dat = qpcr_imputed,
+#                   expr_val = expression) +
+#   scale_y_log10() +
+#   facet_wrap(facets = c('target_name', "species"),
+#              scales = "free_y") +
+#   labs(title = "Marker genes behave as expected [axis y log10]")
+# dev.off()
+# 
+# pdf("../fig/suppl-fig-04b-qpcr-confirms-sampling-scaled.pdf",
+#     height = 6,
+#     width = 6)
+# lineplot_fluidigm(nm = "Sampling Check",
+#                   dat = qpcr_exp,
+#                   expr_val = expr_scale_gene_spec_0_10) +
+#   scale_y_log10() +
+#   labs(title = "Marker genes behave as expected [axis y log10]")
+# dev.off()
